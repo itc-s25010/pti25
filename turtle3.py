a535da664e16330e44fccc9613a1e528f289caae
itc-s25010 <s25010@std.it-college.ac.jp>
2025-06-09 16:59:41 +0900

    20250610

---
+++ turtle3.py
@@ -0,0 +1,7 @@
+from turtle import *
+shape("turtle")
+col = ["orange", "limegreen", "gold", "plum", "tomato"]
+for i in range(5):
+    forward(100)
+    right(144)
+done()
---
+++ turtle5.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape("turtle")
+col = ["orange", "limegreen", "gold", "plum", "tomato"]
+for i in range(99):
+    forward(300)
+    right(135)
+done()
+
